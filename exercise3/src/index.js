77a1e0f2194348856e313cbaea86d0a04e2bf861
Fatemeh <fatemehfaryadras7@gmail.com>
2023-11-11 23:19:19 +0330

    add the final details

--- exercise3/src/index.js
+++ exercise3/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './styles/reset.css';
-import './styles/fonts.css'
+import './styles/fonts.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
