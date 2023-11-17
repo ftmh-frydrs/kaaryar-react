ff8e9aed044397c44921590ae0d003cbeca83870
Fatemeh <fatemehfaryadras7@gmail.com>
2023-11-18 00:20:25 +0330

    nothing

--- exercise3/src/components/Message/index.jsx
+++ exercise3/src/components/Message/index.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
 const Message = ({ id, body, isPeer }) => {
   const messageClass = isPeer ? 'message peer' : 'message self';
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  useEffect(() => {
+    const messageBodyElement = document.getElementById(`message-body-${id}`);
+    const hasOverflow = messageBodyElement.scrollHeight > 150;
+
+    if (hasOverflow) {
+      setIsCollapsed(true);
+    } else {
+      setIsCollapsed(false);
+    }
+  }, [body, id]);
 
   return (
     <div className={messageClass}>
-      <p className="message-body">{body}</p>
+      <p
+        id={`message-body-${id}`}
+        className={`message-body ${isCollapsed ? 'collapsed' : ''}`}
+      >
+        {body}
+      </p>
     </div>
   );
 };
