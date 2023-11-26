72adab53fb76952712e1f4ea947beff0ee77f942
Fatemeh <fatemehfaryadras7@gmail.com>
2023-11-26 23:08:35 +0330

    responsive

--- exercise6/src/components/Comment/index.jsx
+++ exercise6/src/components/Comment/index.jsx
@@ -1,18 +1,23 @@
 import './style.css';
 
-const Comment = ({ comment }) => (
-  <div className='comment-container'>
-    <img src='https://www.w3schools.com/howto/img_avatar.png' alt='' className="avatar" />
-  <div className="comment" style={{ marginLeft: comment.depth * 20 }}>
-    <div className="comment-p">
-    <p className='reply'>reply</p><hr />
-        <p>{comment.text}</p>
+const Comment = ({ comment }) => {
+  // const replyStyle = { marginRight: comment.depth * 20 };
+
+  return (
+    <div className='comment-container'>
+      <img src='https://www.w3schools.com/howto/img_avatar.png' alt='' className="avatar" />
+      <div className="comment">
+        <div className="comment-p">
+          <p className='reply'>reply</p>
+          <hr />
+          <p>{comment.text}</p>
         </div>
-    {comment.replies?.map(reply => (
-      <Comment key={reply.id} comment={reply}/>
-    ))}
-  </div>
-  </div>
-);
+        {comment.replies?.map(reply => (
+          <Comment key={reply.id} comment={reply}/>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Comment;
